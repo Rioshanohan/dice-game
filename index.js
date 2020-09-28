6ac0e3bfc5b3685b27081b42dd26f751ee326e59
Rioshanohan <46582183+Rioshanohan@users.noreply.github.com>
2020-09-28 01:57:16 -0400

    put everything in a function
    A play button could be added and the game would be replayable without reloading the page

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const play = () => {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var imagesrc = "images/dice" + randomNumber1 + ".png";
+  var imagesrc = "images/dice" + randomNumber1 + ".png";
 
-document.querySelectorAll('img')[0].setAttribute("src", imagesrc);
+  document.querySelectorAll('img')[0].setAttribute("src", imagesrc);
 
-var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
-document.getElementById("img02").src = "images/dice" + randomNumber2 + ".png";
+  document.getElementById("img02").src = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelectorAll('h1')[0].innerHTML = "Player 1 wins";
+  if (randomNumber1 > randomNumber2) {
+    document.querySelectorAll('h1')[0].innerHTML = "Player 1 wins";
+  }
+  else if (randomNumber2 == randomNumber1 ) {
+    document.querySelectorAll('h1')[0].innerHTML = "Draw";
+  }
+  else{
+    document.querySelectorAll('h1')[0].innerHTML = "Player 2 wins";
+  }
 }
-else if (randomNumber2 == randomNumber1 ) {
-  document.querySelectorAll('h1')[0].innerHTML = "Draw";
-}
-else{
-  document.querySelectorAll('h1')[0].innerHTML = "Player 2 wins";
-}
+play();
